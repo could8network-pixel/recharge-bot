ced79d3486325e11e4ea47d937517ed205f2e3f2
abin <could8network@gmail.com>
2026-06-25 01:25:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from telegram.ext import Application, CommandHandler
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = "8800314030:AAGfrGLLh9-yCU1pROBrX2LSapMFksYyh-4"
 
-async def start(update, context):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Recharge Bot Online ✅")
 
 app = Application.builder().token(TOKEN).build()
