ce171d29c174ecb53bfe6be7b033f2ee4b5fa8a8
abin <could8network@gmail.com>
2026-06-25 01:48:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,22 +11,15 @@ def webhook():
     sender = request.args.get("from")
     message = request.args.get("message")
 
-    print("Message:", message)
-
     if message and message.lower() == "recharge":
         requests.get(
             "https://whatsbot.tech/api/send_sms",
             params={
                 "api_token": API_TOKEN,
                 "mobile": sender,
-                "message": "📱 Mobile number send",
+                "message": "📱 Mobile number ayakkuka",
                 "device_id": DEVICE_ID
             }
         )
 
     return "OK"
-    if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
