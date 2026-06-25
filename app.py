49ece81ef4481c8cf2b77705337893e17e5d013d
abin <could8network@gmail.com>
2026-06-25 01:14:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return "WhatsBot Webhook Running"
+
+@app.route("/", methods=["POST"])
+def webhook():
+    print("Received:", request.form)
+    return "OK"
