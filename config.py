0d3a31fe22e55e594ddf52afa5f6a9cfeed4bb90
abin <could8network@gmail.com>
2026-06-26 13:22:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
+# WhatsBot
+WHATSBOT_API_TOKEN = "0dba0b41-fa14-4671-9cf7-e1217155218b"
+DEVICE_ID = "46083"
 
+# MystaRec
+MYSTAREC_TOKEN = "YOUR_MYSTAREC_TOKEN"
+
+# Admin WhatsApp Number (91xxxxxxxxxx)
+ADMIN_NUMBER = "917591982334"
+
+# Operator Mapping
+OPERATORS = {
+    "JIO": "RJP",
+    "VI": "VFP",
+    "IDEA": "IDP",
+    "BSNL STV": "BVP",
+    "BSNL TOPUP": "BGP"
+}
